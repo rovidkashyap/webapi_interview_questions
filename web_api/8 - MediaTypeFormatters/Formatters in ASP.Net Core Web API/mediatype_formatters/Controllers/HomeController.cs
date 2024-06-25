83b97a23347da6d209f874060bc2b40608741094
Rovid Kashyap <rvd.kashyap@gmail.com>
2024-06-25 21:15:23 +0530

    Question 8 - MediaTypeFormatters in ASP.Net Core Web API

---
+++ web_api/8 - MediaTypeFormatters/Formatters in ASP.Net Core Web API/mediatype_formatters/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace mediatype_formatters.Controllers
+{
+    // ASP.Net Core Web API includes several built-in media type formatters, which are configured and
+    // utilized through the `AddControllers()` method or extensions like `AddNewtonsoftJson()` for
+    // JSON Handling. Here are main formatters:
+
+    // 1. `SystemTextJsonInputFormatter` and `SystemTextJsonOutputFormatter` - Used for JSON data
+    // 2. `NewtonsoftJsonInputFormatter` and `NewtonsoftJsonOutputFormatter` - Used for JSON data
+    // 3. `XmlSerializerInputFormatter` and `XmlSerializerOutputFormatter` - Used for XML data
+    // 4. `FormUrlEncodedFormatter`
+    // 5.  Custom MediaTypeFormatters
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+    }
+}
