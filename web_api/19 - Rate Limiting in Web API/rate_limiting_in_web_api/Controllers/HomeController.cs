86a6e85fb47890b8c4476868e44575350f7d4c70
Rovid Kashyap <rvd.kashyap@gmail.com>
2024-06-26 17:50:23 +0530

    Rate Limiting in Web API

---
+++ web_api/19 - Rate Limiting in Web API/rate_limiting_in_web_api/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace rate_limiting_in_web_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        /*
+            Implementing rate limiting in an ASP.NET Core Web API helps protect your API from abuse, 
+            ensuring that users do not overwhelm the server with too many requests in a short period. 
+            One of the popular libraries to achieve rate limiting in ASP.NET Core is 
+            `AspNetCoreRateLimit`.
+            
+            1. Install NuGet Package `AspNetCoreRateLimit`.
+            2. Configure Rate Limiting in `appsettings.json`.   - Check `appsettings.json`.
+            3. Configure Services in `Program.cs`.
+            4. Define Rate Limiting Rules. (Check `appsettings.json` file).
+            5. Testing the Rate Limiting
+
+        */
+    }
+}
---
+++ web_api/19 - Rate Limiting in Web API/rate_limiting_in_web_api/Program.cs
@@ -0,0 +1,34 @@
+using AspNetCoreRateLimit;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+// Add Rate Limiting Services
+builder.Services.AddMemoryCache();
+builder.Services.Configure<IpRateLimitOptions>(builder.Configuration.GetSection("IpRateLimiting"));
+builder.Services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
+builder.Services.AddInMemoryRateLimiting();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
